afd874820d5e097218bd29d591eb60d127075bdc
Lummi-GIS <68295520+LummiGIS@users.noreply.github.com>
2021-02-21 12:01:38 -0800

    Add files via upload

--- batch_convert_GPX_to_Shapefile.py
+++ batch_convert_GPX_to_Shapefile.py
@@ -7,18 +7,21 @@ import traceback
 import subprocess
 import shutil
 import os
+from osgeo import ogr
+from osgeo import gdal
 
 try:
     
     #########       USER DEFINED PARAMETERS     ####################
     #This is the path to the folder that stores the Garmin GPX files.  This path can be the actural Garmin connected via mini-USB
-    in_dir = '/home/gerry/PythonScripts/Garmin/GARMIN'
+    in_dir = r"E:\Garmin"
     #This is the directory to store the output shapefiles...
-    out_dir = '/home/gerry/PythonScripts/Garmin/shapefiles'
+    out_dir = r"Z:\GISpublic\GerryG\Python3\Batch_Convert_Garmin_GPX_To_Shapefile\Shapefiles"
     #Mike MacKay gave each Garmin an ID letter.  Enter that here since files from two Garmins might have the same name. This avoids naming conventions and overwriting of files.
     garmin_ID = 'R'
     ################################################################
-    
+   
+    print('Running GPX to SHP converte...')
     
     def confirm_or_make_dir(out_dir):
         '''builds the output directory if it does not already exist'''
@@ -26,7 +29,24 @@ try:
             pass
         else:
             os.mkdir(out_dir)
-    
+            
+    def count_shapefile_records(in_shapefile):
+        '''counts the total number of records in the shapefile...'''
+        #in_shapefile = r"C:\gTemp\CAD_Extents.shp"
+        driver = ogr.GetDriverByName('ESRI Shapefile')
+        dataSource = driver.Open(in_shapefile, 0) # 0 means read-only. 1 means writeable.
+        # Check to see if shapefile is found.
+        if dataSource is None:
+            print('Could not open %s' % (in_shapefile))
+        else:
+            layer = dataSource.GetLayer()
+            featureCount = layer.GetFeatureCount()
+            return featureCount  
+        
+    def delete_empty_shapfile(in_shapefile):
+        driver = ogr.GetDriverByName("ESRI Shapefile")
+        if os.path.exists(in_shapefile):
+            driver.DeleteDataSource(in_shapefile)
     
     #confirm or build the output directory
     confirm_or_make_dir(out_dir)
@@ -44,15 +64,29 @@ try:
                 if in_file == '.Position.gpx':
                     pass
                 else:
+                    #make a backup of the gpx files
+                    
                     #Build the file output paths as string
                     file_name_only = in_file.split('.')[0]
-                    this_dir = out_dir  + '/Garmin_Unit_' + garmin_ID + '_' + file_name_only +'.shp'
-                    #Build the ogr command...
-                    cmd = 'ogr2ogr -f "ESRI Shapefile" '+ ' "' + this_dir  + '" "' + fullpath + '"'
-                    print('running', cmd)
-                    #Run the command from the command line.
-                    subprocess.call(cmd, shell = 'True')                
-
+                    #this_dir = out_dir  + '/Garmin_Unit_' + garmin_ID + '_' + file_name_only +'.shp'
+                    this_dir = out_dir  + '/Garmin_Unit_' + garmin_ID + '_' + file_name_only 
+                    gdal.VectorTranslate(this_dir, fullpath)
+                    shutil.move(fullpath, out_dir)
+    #Garmin GPX converts to at least five files, many of which are empty. If so, purge them.                
+    for (dirpath, dirnames, filenames) in os.walk(out_dir):
+        for in_file in filenames:
+            if in_file.endswith('.shp') or in_file.endswith('.SHP'):
+                if dirpath == in_dir:
+                    fullpath = os.path.join(dirpath,in_file)
+                else:
+                    fullpath = os.path.join(dirpath,in_file)                        
+                    record_count = count_shapefile_records(fullpath)
+                    if record_count == 0:
+                        delete_empty_shapfile(fullpath)
+                        
+                        
+                
+    print('Finished without error')
 except:
     tb = sys.exc_info()[2]
     tbinfo = traceback.format_tb(tb)[0]
